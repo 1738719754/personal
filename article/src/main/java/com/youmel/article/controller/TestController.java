a397a5b721850bb0018f3ef77d6cb08553d5e206
chenli <1738719754@qq.com>
2017-09-19 15:27:25 +0800

    项目搭建：集成bus消息总线

---
+++ article/src/main/java/com/youmel/article/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.youmel.article.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by CDDF on 2017/9/8.
+ */
+@RefreshScope
+@RestController
+public class TestController {
+    @Value("${from}")
+    private String from;
+    @RequestMapping("test")
+    public String test(){
+        return from;
+    }
+}
